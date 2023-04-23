950f02d7e1417ed037a07de56f410309a5fe9a30
lmx <lmx@1123377679.com>
2023-04-23 23:19:34 +0800

    李明响提交的代码

--- src/main/java/lmx.java
+++
@@ -1,5 +0,0 @@
-public class lmx {
-    public static void main(String[] args) {
-        System.out.println("熊思墁修改lmx的代码");
-    }
-}
--- src/main/java/xsm.java
+++
@@ -1,5 +0,0 @@
-public class xsm {
-    public static void main(String[] args) {
-
-    }
-}
